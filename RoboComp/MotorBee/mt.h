f1ebd6ea084c0250d4287b12140ed640bd8935be
Shahin Gasanov <59098578+shgasanov@users.noreply.github.com>
2023-03-12 05:30:21 +0200

    Delete RoboComp directory

--- RoboComp/MotorBee/mt.h
+++
@@ -1,5 +0,0 @@
-// Header file for use with mtb.dll
-
-typedef bool	(*Type_InitMotoBee)();								
-typedef bool	(*Type_SetMotors)(int on1, int speed1, int on2, int speed2, int on3, int speed3, int on4, int speed4, int servo);
-typedef void	(*Type_Digital_IO)(int *inputs, int outputs);
